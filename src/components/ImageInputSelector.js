23bb82dcdfaebf001329556cea4681faadb1aa2d
chrisdennett <chrisdennett@gmail.com>
2020-09-13 17:21:17 +0100

    Added a sample image loading option

--- src/components/ImageInputSelector.js
+++ src/components/ImageInputSelector.js
@@ -1,11 +1,20 @@
 import React from "react";
+import SampleImageLoader from "./SampleImageLoader";
 import { WebcamCapture } from "./WebcamCapture";
 
 const ImageInputSelector = ({ setSrcImg, setFrameNumber }) => {
+  const currentInput = "sample";
+
   return (
     <div>
       <h1>ImageInputSelector</h1>
-      <WebcamCapture setSrcImg={setSrcImg} setFrameNumber={setFrameNumber} />
+      {currentInput === "sample" && (
+        <SampleImageLoader imgName={"tizziewizie.jpg"} setSrcImg={setSrcImg} />
+      )}
+
+      {currentInput === "webcam" && (
+        <WebcamCapture setSrcImg={setSrcImg} setFrameNumber={setFrameNumber} />
+      )}
     </div>
   );
 };
--- src/components/SampleImageLoader.js
+++ src/components/SampleImageLoader.js
@@ -1,6 +1,19 @@
-import React from "react";
+import React, { useEffect } from "react";
 
-const SampleImageLoader = () => {
+const SampleImageLoader = ({ srcImg, setSrcImg, imgName }) => {
+  useEffect(() => {
+    if (!srcImg) {
+      const image = new Image();
+      image.crossOrigin = "Anonymous";
+      image.onload = () => {
+        console.log("image: ", image);
+        setSrcImg(image);
+      };
+      image.src = imgName;
+    }
+    // eslint-disable-next-line
+  }, [imgName]);
+
   return (
     <div>
       <h1>SampleImageLoader</h1>
